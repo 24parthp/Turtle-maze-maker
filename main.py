049006623d4b74f2d472d079964cef86ff59d222
Parth Patel <24patepar@molineschools.org>
2024-02-26 13:04:15 -0600

    make wall

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import turtle as t
 
-wn = t.screen()
+wn = t.Screen()
+wn.setup(500,500)
 
 mazePointer = t.Turtle()
+mazePointer.pensize(8)
 
+def startingPosition(pointer):
+    pointer.penup()
+    pointer.seth(270)
+    pointer.goto(200,200)
+    pointer.pendown()
 
+def drawMaze(pointer):
+    len = 400
+    num_of_walls = 21
+    wall_distance = 20
+
+    def wall(distance):
+        pointer.fd(distance)
+
+    for i in range(num_of_walls):
+        wall(len)
+        len-= wall_distance
+        pointer.right(90)
+
+startingPosition(mazePointer)
+drawMaze(mazePointer)
 
 wn.mainloop()
