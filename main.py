f5fcc59fd04e1675dc92412ff1293d5bd36800ae
Parth Patel <parthvapi2005@gmail.com>
2024-03-02 15:15:05 -0600

    add powerup window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle as t
 import random as rand
-import threading
+import math
+from tkinter import *
 
 wn = t.Screen()
 wn.setup(500,500)
@@ -9,6 +10,7 @@ mazePointer, playerPointer = t.Turtle(), t.Turtle()
 mazePointer.pensize(8)
 mazePointer.speed(0) , playerPointer.speed(0)
 
+powerup_amount = 0
 
 def drawMaze(pointer):
     len = 400
@@ -47,14 +49,7 @@ def drawMaze(pointer):
 
             pointer.fd(len-pathway)
     
-    def powerup():
-        xpos = rand.randint(-190, 190)
-        ypos = rand.randint(-210, 150)
-        pointer.penup()
-        pointer.goto(xpos, ypos)
-        pointer.pendown()
-        pointer.color("red")
-        pointer.circle(2)
+    
 
     startingPosition(pointer)
 
@@ -63,20 +58,34 @@ def drawMaze(pointer):
         len-= wall_distance
         pointer.right(90)
 
-    powerup()
+class powerup:
+    def __init__(self, pointer):
+        self.xpos, self.ypos =  rand.randint(-190, 190), rand.randint(-210, 150)
+        self.pointer = pointer
+
+    def draw(self):
+        self.pointer.penup()
+        self.pointer.goto(self.xpos, self.ypos)
+        self.pointer.pendown()
+        self.pointer.color("red")
+        self.pointer.circle(2)
+    
+    # def capture(self):
 
 def player(pointer):
     movementSpeed = 10
 
     def startingPosition():
-        pointer.hideturtle()
         pointer.penup()
         pointer.goto(0, 200)
-        pointer.pendown()
         pointer.color("blue")
+        pointer.shape('circle')
+        pointer.shapesize(0.5, 0.5)
         pointer.pensize(10)
-        pointer.circle(5)
-        
+    
+    def dist(x1, y1, x2, y2):
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    
     def move(key):
         if key == 'w':
             pointer.seth(90)
@@ -90,16 +99,39 @@ def player(pointer):
         if key == 'd':
             pointer.seth(0)
             pointer.fd(movementSpeed)
+
+        if(dist(pointer.xcor(), pointer.ycor(), powerup1.xpos, powerup1.ypos) < 11):
+            print('yes it works')
+
+        wn.ontimer(lambda: update_powerup_label(label2), 100)
     
-    wn.onkey(lambda: move('w'), 'w')
-    wn.onkey(lambda: move('a'), 'a')
-    wn.onkey(lambda: move('s'), 's')
-    wn.onkey(lambda: move('d'), 'd')
-    
+    wn.onkeypress(lambda: move('w'), 'w')
+    wn.onkeypress(lambda: move('a'), 'a')
+    wn.onkeypress(lambda: move('s'), 's')
+    wn.onkeypress(lambda: move('d'), 'd')
     
     startingPosition()
 
+def powerupWn():
+    global label2
+
+    window = Tk()
+    window.geometry('200x100+400+400')
+
+    label = Label(window, text='Number of Powerups:', font=('Arial', 15))
+    label2 = Label(window, text=str(powerup_amount), font=('Arial', 25))
+
+    label.pack()
+    label2.pack()
+
+def update_powerup_label(label):
+    label.config(text=str(powerup_amount))
+
+powerup1 = powerup(mazePointer)
+
 drawMaze(mazePointer)
+powerup1.draw()
+powerupWn()
 player(playerPointer)
 
 wn.listen()
